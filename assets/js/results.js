fb836373a2a3bcf2899dd730e736fc9a2ed270fb
akaur091 <amniderkaur.09@gmail.com>
2022-04-13 13:20:00 -0700

    add functions to script.js for cardselector

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,8 +1,11 @@
 console.log("this is the results page")
 
 // fetch request
-var apiKey = ''
-var targetURL =
+// concatenate choice from wheel page into the targetURL
+
+var apiKey = 'AIzaSyA6xeHz0LvbZvjRV2pp1eA00sbEjms-b1M'
+var targetURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=restaurant&inputtype=textquery&key=" +apiKey
+console.log(targetURL)
     fetch(targetURL)
     .then(response => response.json())
     .then(data => console.log(data));
