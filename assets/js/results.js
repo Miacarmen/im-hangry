090eff0904c8ae22fade602847de27e9a435b6aa
CT <tellezc007@gmail.com>
2022-04-13 11:21:22 -0700

    add fetch request layout

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,1 +1,8 @@
 console.log("this is the results page")
+
+// fetch request
+var apiKey = ''
+var targetURL =
+    fetch(targetURL)
+    .then(response => response.json())
+    .then(data => console.log(data));
