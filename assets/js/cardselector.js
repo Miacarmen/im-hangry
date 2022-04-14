aec820820b9b86348e6f57432266b5fa07f17a68
miacarmen <maitreyacarmen@gmail.com>
2022-04-14 15:49:36 -0700

    number of cards displayed based on number of input selections, some additional page styling added

--- assets/js/cardselector.js
+++ assets/js/cardselector.js
@@ -1,49 +1,43 @@
+var stored = localStorage.getItem("selected");
+console.log(stored);
+var cuisine = JSON.parse(stored);
 
-// fuction to create the card
-
-
-
-
-var cardContainer = document.getElementById('card-container');
-var card = document.createElement('div');
-card.classList.add('card-down');
-card.textContent = 'card';
-cardContainer.appendChild(card);
-
-
+for (let i = 0; i <= cuisine.length; i++) {
+  var cardContainer = document.getElementById("card-container");
+  var card = document.createElement("div");
+  card.classList.add("card-down");
+  card.textContent = "card";
+  cardContainer.appendChild(card);
+}
 
 
 
 // function for card shuffle
-function shuffleCards(){
-    // have selection of food come here to shuffle in cards
-    // grabs selected variable from local storage
-    let selected = ["American", "Argentinian", "BBQ"];
-    // let wordArray =["image-1","image-1","image-2","image-2"];
-    // let wordArray = ["American", "Argentinian", "BBQ","Brazilian", "Burgers", "British","Burgers", "Caribbean","Cajun","Chinese","Ethipoian","Filipino","French","Greek","Hawaiian","Indian","Italian","Japanese","Korean","Korean BBQ","Lebanese","Mexican","Morocaan","Middle Eastern","Pasta","Persian","Pizza","Pho","Ramen","Salad","Sandwiches","Seafood","Soup","Tacos","Taiwanese","Thai","Vegan","Vegetarian","Vietnamese"];
-    for (var i = selected.length -1; i >0; i--){
-        var j =Math.floor(Math.random() * (i+1));
-        var temp = selected[i];
-        selected[i] = selected[j]
-        selected[j] = temp;  
-    } 
-    return;
+function shuffleCards() {
+  // have selection of food come here to shuffle in cards
+  // grabs selected variable from local storage
+  let selected = ["American", "Argentinian", "BBQ"];
+  // let wordArray =["image-1","image-1","image-2","image-2"];
+  // let wordArray = ["American", "Argentinian", "BBQ","Brazilian", "Burgers", "British","Burgers", "Caribbean","Cajun","Chinese","Ethipoian","Filipino","French","Greek","Hawaiian","Indian","Italian","Japanese","Korean","Korean BBQ","Lebanese","Mexican","Morocaan","Middle Eastern","Pasta","Persian","Pizza","Pho","Ramen","Salad","Sandwiches","Seafood","Soup","Tacos","Taiwanese","Thai","Vegan","Vegetarian","Vietnamese"];
+  for (var i = selected.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = selected[i];
+    selected[i] = selected[j];
+    selected[j] = temp;
+  }
+  return;
 }
-
-
-
 
 // change to jquery onClick to flip card
-function cardFlipWhenClicked(cardObjects){
-    cardObjects.element = function (){
-        // add event listener here for on  click response
-        if (cardObjects.element.classList.contains("flipped")){
-            return;
-        }
-        cardObjects.element.classList.add("flipped");
+function cardFlipWhenClicked(cardObjects) {
+  cardObjects.element = function () {
+    // add event listener here for on  click response
+    if (cardObjects.element.classList.contains("flipped")) {
+      return;
     }
+    cardObjects.element.classList.add("flipped");
+  };
 }
-
 
 // set up cards
 // let cardObjects =
@@ -54,27 +48,25 @@ function cardFlipWhenClicked(cardObjects){
 //     }
 // }
 
-
-
 // add a re-shuffle button
-$('#reshufflebtn').click(function(event) {
-    event.preventDefault();
-    console.log('button clicked');
-    // invoke shuffle function (using jquery)
+$("#reshufflebtn").click(function (event) {
+  event.preventDefault();
+  console.log("button clicked");
+  // invoke shuffle function (using jquery)
 });
 
 // add a reselect button
-$('#reselectbtn').click(function(event) {
-    event.preventDefault();
-    console.log('button clicked');
-    // link to resultspage.html
-    window.location.href='index.html';
+$("#reselectbtn").click(function (event) {
+  event.preventDefault();
+  console.log("button clicked");
+  // link to resultspage.html
+  window.location.href = "index.html";
 });
 
 // add on click event listener to resultbtn
-$('#resultbtn').click(function(event) {
-    event.preventDefault();
-    console.log('button clicked');
-    // link to resultspage.html
-    window.location.href='resultsPage.html';
+$("#resultbtn").click(function (event) {
+  event.preventDefault();
+  console.log("button clicked");
+  // link to resultspage.html
+  window.location.href = "resultsPage.html";
 });
