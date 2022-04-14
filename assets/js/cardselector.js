7b6e16821bf2239e10aa653a38f38c7021d3906d
akaur091 <amniderkaur.09@gmail.com>
2022-04-14 08:39:17 -0700

    add notes to card selector js to figure out how to add btns

--- assets/js/cardselector.js
+++ assets/js/cardselector.js
@@ -11,19 +11,27 @@ function appendNewCard(parentCard){
 }
 // function for card shuffle
 function shuffleCards(){
-    let wordArray = ["American", "Argentinian", "BBQ","Brazilian", "Burgers", "British","Burgers", "Caribbean","Cajun","Chinese","Ethipoian","Filipino","French","Greek","Hawaiian","Indian","Italian","Japanese","Korean","Korean BBQ","Lebanese","Mexican","Morocaan","Middle Eastern","Pasta","Persian","Pizza","Pho","Ramen","Salad","Sandwiches","Seafood","Soup","Tacos","Taiwanese","Thai","Vegan","Vegetarian","Vietnamese"];
-    return wordArray.shuffle(wordArray);
+    // have selection of food come here to shuffle in cards
+    let wordArray =[];
+    // let wordArray = ["American", "Argentinian", "BBQ","Brazilian", "Burgers", "British","Burgers", "Caribbean","Cajun","Chinese","Ethipoian","Filipino","French","Greek","Hawaiian","Indian","Italian","Japanese","Korean","Korean BBQ","Lebanese","Mexican","Morocaan","Middle Eastern","Pasta","Persian","Pizza","Pho","Ramen","Salad","Sandwiches","Seafood","Soup","Tacos","Taiwanese","Thai","Vegan","Vegetarian","Vietnamese"];
+    for (var i = wordArray.length -1; i >0; i--){
+        var j =Math.floor(Math.random() * (i+1));
+        var temp = wordArray[i];
+        wordArray[i] = wordArray[j]
+        wordArray[j] = temp;  
+    } 
+    return;
 }
 // fuction to create the card
 function createCards(parentCard, shuffleCards){
     let cardlist =[];
     for (let i = 0; i<= 11; i++){
         let newCard = appendNewCard(parentCard);
-        newCard.classList.add(shuffleCards[i]);
+        newCard.classList.add(shuffleCards);
         var cardObjects ={
             index: 1,
             element: newCard,
-            imageclass: shuffleCards[i],
+            imageclass: shuffleCards,
         };
         cardlist.push(cardObjects);
     }
@@ -38,7 +46,8 @@ function incrementCounter(counterName, parentCard){
     parentCard.innerHTML = counters[counterName];
 }
 function cardFlipWhenClicked(cardObjects){
-    cardObjects.element.onclick = function (){
+    cardObjects.element = function (){
+        // add event listener here for on  click response
         if (cardObjects.element.classList.contains("flipped")){
             return;
         }
@@ -50,5 +59,9 @@ function cardFlipWhenClicked(cardObjects){
 let cardObjects =
     createCards(document.getElementById("card-container"), shuffleCards());
 if (cardObjects !=null){
-    
+    for (let i = 0; i < cardObjects.length; i++){
+        cardFlipWhenClicked(cardObjects);
+    }
 }
+
+// add a re-do button
