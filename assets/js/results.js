03a80b428101b174506d4dd671e5d452b446e72e
sharkbyte <sgquin@gmail.com>
2022-04-14 10:35:16 -0700

    moved spoonfetch to resultspage

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,22 +1,9 @@
 console.log("this is the results page")
-//
-// fetch request
-// concatenate choice from wheel page into the targetURL
 
-// var apiKey = 'AIzaSyA6xeHz0LvbZvjRV2pp1eA00sbEjms-b1M'
-// var targetURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyA6xeHz0LvbZvjRV2pp1eA00sbEjms-b1M&input=restaurant&inputtype=textquery&fields=name&locationbias=ipbias"
-// console.log(targetURL)
-// fetch(targetURL,{
-// })
-//   .then((resp)=>{
-//     console.log(resp)
-//     return resp.json()
-//   })
-//   .then(data => console.log(data))
-//
-//
+//geoLocation to get latLon of user
 var lat = ''
 var lon = ''
+
 var options = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -32,18 +19,36 @@ function success(pos) {
   lon= crd.longitude
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
-  console.log(lat)
-  console.log(lon)
-  fetchByLtLn(lat,lon)
+  saveLatLon(lat,lon)
 }
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
-
 navigator.geolocation.getCurrentPosition(success, error, options);
 
+//waits for geoLocation to be success
+function saveLatLon(x,y){
+  lat = x
+  lon = y
+  console.log(lat)
+  console.log(lon)
+}
 
+
+function spoonFetch(){
+var targetURL = "https://api.spoonacular.com/recipes/complexSearch?cuisine=indian&apiKey=446369fd8c0b4e3eb39992c76f883a83"
+fetch(targetURL)
+    .then(function(response) {
+        console.log(response)
+        return response.json()
+    })
+    .then(data => console.log(data))
+}
+
+
+
+function taFetch(x,y){
 const opts = {
 	method: 'GET',
 	headers: {
@@ -51,10 +56,7 @@ const opts = {
 		'X-RapidAPI-Key': '644883e55amsh23c137fe60742eep16527bjsne5d9552c9387'
 	}
 };
-console.log(lat)
-console.log(lon)
 
-function fetchByLtLn(x,y){
   var fetchURL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng?latitude='+x+'&longitude='+y+'&limit=30&currency=USD&distance=2&open_now=false&lunit=km&lang=en_US'
 
 fetch(fetchURL, opts)
