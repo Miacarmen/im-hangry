3454a1c103d32824b60cae4baa1a247a5706a82d
miacarmen <maitreyacarmen@gmail.com>
2022-04-14 23:18:17 -0700

    pointless commit

--- assets/js/cardselector.js
+++ assets/js/cardselector.js
@@ -2,20 +2,26 @@ var stored = localStorage.getItem("selected");
 var cuisine = JSON.parse(stored);
 
 for (let i = 0; i < cuisine.length; i++) {
-  var cardContainer = document.getElementById("card-container");
+//   var cardContainer = document.getElementById("card-container");
   var card = document.createElement("div");
 
-  card.classList.add("card-down");
-
+  var cardcontainer = $('#card-container').append(card);
+  card.classList.add("card-down, card-up");
+  card.classList.add("card-up");
+  card.textContent = cuisine[i];
   //   $(card).append($(`<img src='./assets/pics/cardimage.png' width='125px' height='175px'>`));
-  cardContainer.appendChild(card);
+  cardcontainer.appendChild(card);
   //   adding background image to card
 
-  card.classList.add("card-up");
+  
   // add text content to each card if class = card-up
 }
 
 // $(`<img src='./assets/pics/cardimage.png'>`).appendTo(".card-down");
+$(card).click(function() {
+    card.classList.add('card-up');
+    card.classList.remove('card-down');
+});
 
 // function for card shuffle
 function shuffleCards(arr) {
