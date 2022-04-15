b2130451f1f4df5436d766242dbd3cd9d783a647
miacarmen <maitreyacarmen@gmail.com>
2022-04-14 21:23:37 -0700

    resolved merge conflicts

--- assets/js/cardselector.js
+++ assets/js/cardselector.js
@@ -5,7 +5,6 @@ var cuisine = JSON.parse(stored);
 
 
 
-
 for (let i = 0; i < cuisine.length; i++) {
   var cardContainer = document.getElementById("card-container");
   var card = document.createElement("div");
